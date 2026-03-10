fe1b83e870856b3f61fde648a0e330fe824ae29e
andrey828 <andreymanea03@gmail.com>
2026-03-10 19:57:42 +0100

    Add HUD group for UltraHUD in AddonTemplate

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -2,6 +2,7 @@ package com.example.addon;
 
 import com.example.addon.modules.LanzaDMG;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
@@ -10,12 +11,15 @@ import org.slf4j.LoggerFactory;
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LoggerFactory.getLogger(AddonTemplate.class);
     public static final Category CATEGORY = new Category("UltraAddon");
+    
+    // Esto es lo que te pide el error del HUD
+    public static final HudGroup HUD_GROUP = new HudGroup("UltraHUD");
 
     @Override
     public void onInitialize() {
         LOG.info("Inicializando UltraAddon...");
 
-        // Registro de módulos
+        // Registro de tu módulo
         Modules.get().add(new LanzaDMG());
     }
 
