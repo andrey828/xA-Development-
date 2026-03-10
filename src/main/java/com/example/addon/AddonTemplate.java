10691e4d78982893bb855e433fae8a6c5a63cf1c
andrey828 <andreymanea03@gmail.com>
2026-03-10 21:34:53 +0100

    Refactor AddonTemplate category and logging messages

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -2,24 +2,23 @@ package com.example.addon;
 
 import com.example.addon.modules.LanzaDMG;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.item.Items; // Import extra para seguridad
 
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LoggerFactory.getLogger(AddonTemplate.class);
-    public static final Category CATEGORY = new Category("UltraAddon");
     
-    // Esto es lo que te pide el error del HUD
-    public static final HudGroup HUD_GROUP = new HudGroup("UltraHUD");
+    // Cambiamos el nombre de la categoría a algo simple para probar
+    public static final Category CATEGORY = new Category("Ultra");
 
     @Override
     public void onInitialize() {
-        LOG.info("Inicializando UltraAddon...");
+        LOG.info("Cargando UltraAddon en 1.21.4...");
 
-        // Registro de tu módulo
+        // Registro seguro
         Modules.get().add(new LanzaDMG());
     }
 
