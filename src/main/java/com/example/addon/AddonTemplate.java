bc49f1692e2092e6c159e7918e7fde9f43a624fd
xAndrey <andreymanea03@gmail.com>
2026-03-12 17:05:54 +0100

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -7,20 +7,20 @@ import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.item.Items;
 
 public class AddonTemplate extends MeteorAddon {
-    public static final Category CATEGORY = new Category("xAddon", Items.DIAMOND_SWORD.getDefaultStack());
+    public static final Category CATEGORY = new Category("xAddon", Items.MACE.getDefaultStack());
 
     @Override
     public void onInitialize() {
-        // Aquí NO registramos la categoría, solo añadimos los módulos
         Modules m = Modules.get();
+        
         m.add(new SuperAura());
+        m.add(new SuperTotem());
         m.add(new TotemGuard());
         m.add(new LanzaDMG());
     }
 
     @Override
     public void onRegisterCategories() {
-        // ESTO es lo que pide el crash. El registro va AQUÍ.
         Modules.registerCategory(CATEGORY);
     }
 
