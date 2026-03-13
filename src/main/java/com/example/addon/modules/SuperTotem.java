2fb3348dd0d98a50ba9b0add7c2a1a68bc54f101
xAndrey <andreymanea03@gmail.com>
2026-03-13 01:55:52 +0100

    Update SuperTotem.java

--- src/main/java/com/example/addon/modules/SuperTotem.java
+++ src/main/java/com/example/addon/modules/SuperTotem.java
@@ -2,67 +2,41 @@ package com.example.addon.modules;
 
 import com.example.addon.AddonTemplate;
 import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.utils.player.FindItemResult;
-import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.orbit.EventHandler;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.screen.slot.SlotActionType;
 
 public class SuperTotem extends Module {
-    private final SettingGroup sgGeneral = settings.getDefaultGroup();
-
-    private final Setting<Boolean> mainHand = sgGeneral.add(new BoolSetting.Builder()
-        .name("main-hand")
-        .defaultValue(true)
-        .build()
-    );
-
-    private int delay;
-
     public SuperTotem() {
-        super(AddonTemplate.CATEGORY, "SuperTotem", "AutoTotem estable sin parpadeos.");
+        super(AddonTemplate.CATEGORY, "super-totem", "AutoTotem Pero Mejor.");
     }
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
-        if (mc.player == null || mc.currentScreen != null) return;
+        if (mc.player == null || mc.interactionManager == null) return;
 
-        if (delay > 0) {
-            delay--;
-            return;
-        }
+        if (mc.player.getOffHandStack().getItem() == Items.TOTEM_OF_UNDYING) return;
 
-        if (mc.player.getOffHandStack().getItem() != Items.TOTEM_OF_UNDYING) {
-            FindItemResult totem = InvUtils.find(Items.TOTEM_OF_UNDYING);
-            if (totem.found()) {
-                InvUtils.move().from(totem.slot()).toOffhand();
-                delay = 2; 
-                return;
-            }
-        }
-
-        if (mainHand.get() && mc.player.getMainHandStack().getItem() != Items.TOTEM_OF_UNDYING) {
-            FindItemResult totem = InvUtils.find(i -> i.getItem() == Items.TOTEM_OF_UNDYING && i != mc.player.getOffHandStack());
-            
-            if (totem.found()) {
-                int handSlot = -1;
-                ItemStack handStack = mc.player.getMainHandStack();
-                
-                for (int i = 0; i < 9; i++) {
-                    if (mc.player.getInventory().getStack(i) == handStack) {
-                        handSlot = i;
-                        break;
-                    }
-                }
-
-                if (handSlot != -1) {
-                    InvUtils.move().from(totem.slot()).to(handSlot);
-                    delay = 2;
-                }
-            }
+        int totemSlot = findTotem();
+        if (totemSlot != -1) {
+            mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, totemSlot, 0, SlotActionType.PICKUP, mc.player);
+            mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, 45, 0, SlotActionType.PICKUP, mc.player);
+            mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, totemSlot, 0, SlotActionType.PICKUP, mc.player);
         }
     }
-}
 
+    private int findTotem() {
+        for (int i = 9; i < 45; i++) {
+            if (mc.player.getInventory().getStack(i).getItem() == Items.TOTEM_OF_UNDYING) {
+                return i;
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            if (mc.player.getInventory().getStack(i).getItem() == Items.TOTEM_OF_UNDYING) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
