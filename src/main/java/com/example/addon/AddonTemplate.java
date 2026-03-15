34cf34bbe1fe60b3baa056d2422642fd6c686b5a
xAndrey <andreymanea03@gmail.com>
2026-03-15 18:04:40 +0100

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -20,6 +20,7 @@ public class AddonTemplate extends MeteorAddon {
         m.add(new FareWell());
         m.add(new xABackstep());
         m.add(new xArmor());
+        m.add(new MegaAutoTotem());
     }
 
     @Override
