0aacd7f75a636358fcfb0d4ed803a6853227c8d4
xAndrey <andreymanea03@gmail.com>
2026-03-17 17:06:46 +0100

    Update xRPC.java

--- src/main/java/com/example/addon/modules/xRPC.java
+++ src/main/java/com/example/addon/modules/xRPC.java
@@ -4,40 +4,33 @@ import com.example.addon.AddonTemplate;
 import meteordevelopment.discordipc.DiscordIPC;
 import meteordevelopment.discordipc.RichPresence;
 import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.gui.utils.StarscriptTextBoxRenderer;
 import meteordevelopment.meteorclient.settings.IntSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.settings.StringListSetting;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.utils.misc.MeteorStarscript;
 import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
-import meteordevelopment.starscript.Script;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class RPC extends Module {
+public class xRPC extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
     private final Setting<List<String>> l1 = sgGeneral.add(new StringListSetting.Builder()
-        .name("Line 1")
-        .defaultValue(List.of("Playing on {server}", "{player}"))
-        .renderer(StarscriptTextBoxRenderer.class)
+        .name("line-1")
+        .defaultValue(List.of("xA Addon on top", "xA Development"))
         .build()
     );
     private final Setting<List<String>> l2 = sgGeneral.add(new StringListSetting.Builder()
-        .name("Line 2")
-        .defaultValue(List.of("{server.player_count} Players online", "{round(player.health, 1)}hp"))
-        .renderer(StarscriptTextBoxRenderer.class)
+        .name("line-2")
+        .defaultValue(List.of("Using xA modules", "Best Addon"))
         .build()
     );
     private final Setting<Integer> refreshDelay = sgGeneral.add(new IntSetting.Builder()
-        .name("Refresh Delay")
+        .name("refresh-delay")
         .defaultValue(100)
-        .range(0, 1000)
-        .sliderRange(0, 1000)
+        .min(0)
         .build()
     );
 
@@ -46,11 +39,9 @@ public class RPC extends Module {
     private int index2 = 0;
     private static final RichPresence presence = new RichPresence();
 
-    public RPC() {
+    public xRPC() {
         super(AddonTemplate.CATEGORY, "xRPC", "Discord Rich Presence for xA.");
-        
-        // Esto hace que se active automáticamente al cargar el addon
-        if (!isActive()) toggle(); 
+        if (!isActive()) toggle();
     }
 
     @Override
@@ -76,30 +67,21 @@ public class RPC extends Module {
 
     public void updatePresence() {
         ticks = refreshDelay.get();
-        List<String> messages1 = getMessages(l1.get());
-        List<String> messages2 = getMessages(l2.get());
+        List<String> messages1 = l1.get();
+        List<String> messages2 = l2.get();
         
         if (messages1.isEmpty() || messages2.isEmpty()) return;
 
         index1 = (index1 + 1) % messages1.size();
         index2 = (index2 + 1) % messages2.size();
 
-        presence.setDetails(mc.player == null ? "In Main Menu" : messages1.get(index1));
-        presence.setState(mc.player == null ? "In Main Menu" : messages2.get(index2));
+        String line1 = mc.player == null ? "In Main Menu" : messages1.get(index1);
+        String line2 = mc.player == null ? "In Main Menu" : messages2.get(index2);
+
+        presence.setDetails(line1);
+        presence.setState(line2);
         presence.setLargeImage("25565", "xA Addon");
         
         DiscordIPC.setActivity(presence);
     }
-
-    private List<String> getMessages(List<String> stateList) {
-        List<String> messages = new ArrayList<>();
-        for (String msg : stateList) {
-            Script script = MeteorStarscript.compile(msg);
-            if (script != null) {
-                messages.add(MeteorStarscript.run(script).toString());
-            }
-        }
-        return messages;
-    }
 }
-
