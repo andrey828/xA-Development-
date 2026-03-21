9f886cf7c2b0f5b898e5981fcf365c242318786e
xAndrey <andreymanea03@gmail.com>
2026-03-21 16:42:14 +0100

    Update TotemGuard.java

--- src/main/java/com/example/addon/modules/TotemGuard.java
+++ src/main/java/com/example/addon/modules/TotemGuard.java
@@ -2,41 +2,26 @@ package com.example.addon.modules;
 
 import com.example.addon.AddonTemplate;
 import com.example.addon.mixin.accessor.PlayerMoveC2SPacketAccessor;
-
-import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
-
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
-public class TotemGuard extends Module {
+public class TotemGuarld extends Module {
 
-    public TotemGuard() {
-        super(AddonTemplate.CATEGORY, "xNoPoP", "Cancels fall damage.");
+    public TotemGuarld() {
+        super(AddonTemplate.CATEGORY, "xTotem", "Cancels all fall damage, even with fly.");
     }
 
+     //papoi el q lo lea 
+    //q miras aki ? 
+    
     @EventHandler
-    private void onTick(TickEvent.Pre event) {
-        if (mc.player == null || mc.getNetworkHandler() == null) return;
+    public void onSendPacket(PacketEvent.Send event) {
+        if (mc.player == null) return;
 
-        // Detectar caída
-        if (mc.player.fallDistance > 2 || mc.player.getVelocity().y < -0.1) {
-
-            PlayerMoveC2SPacket.PositionAndOnGround packet =
-                new PlayerMoveC2SPacket.PositionAndOnGround(
-                    mc.player.getX(),
-                    mc.player.getY(),
-                    mc.player.getZ(),
-                    true,
-                    mc.player.horizontalCollision
-                );
-
-            // Usa TU mixin
-            ((PlayerMoveC2SPacketAccessor) packet).setOnGround(true);
-
-            mc.getNetworkHandler().sendPacket(packet);
-
-            // Reset caída
+        if (event.packet instanceof PlayerMoveC2SPacket movePacket) {
+            ((PlayerMoveC2SPacketAccessor) movePacket).setOnGround(false);
             mc.player.fallDistance = 0f;
         }
     }
