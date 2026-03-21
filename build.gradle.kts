68d6b0ec1d42919d67afaba1cb4be2060c89104f
xAndrey <andreymanea03@gmail.com>
2026-03-21 21:41:41 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 plugins {
     alias(libs.plugins.fabric.loom)
 }
-
 base {
     archivesName = properties["archives_base_name"] as String
     version = libs.versions.mod.version.get()
     group = properties["maven_group"] as String
 }
-
 repositories {
     maven {
         name = "meteor-maven"
@@ -17,47 +15,43 @@ repositories {
         name = "meteor-maven-snapshots"
         url = uri("https://maven.meteordev.org/snapshots")
     }
+    maven {
+        name = "jitpack"
+        url = uri("https://jitpack.io")
+    }
 }
-
 dependencies {
     // Fabric
     minecraft(libs.minecraft)
     mappings(variantOf(libs.yarn) { classifier("v2") })
     modImplementation(libs.fabric.loader)
-
     // Meteor
     modImplementation(libs.meteor.client)
+    // discord-ipc incluida explicitamente para poder modificarla
+    include(implementation("com.github.MeteorDevelopment:discord-ipc:2be0a88")!!)
 }
-
 tasks {
     processResources {
         val propertyMap = mapOf(
             "version" to project.version,
             "mc_version" to libs.versions.minecraft.get()
         )
-
         inputs.properties(propertyMap)
-
         filteringCharset = "UTF-8"
-
         filesMatching("fabric.mod.json") {
             expand(propertyMap)
         }
     }
-
     jar {
         inputs.property("archivesName", project.base.archivesName.get())
-
         from("LICENSE") {
             rename { "${it}_${inputs.properties["archivesName"]}" }
         }
     }
-
     java {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
-
     withType<JavaCompile> {
         options.encoding = "UTF-8"
         options.release = 21
