c32fb03a82458c74d2cd7418078faf236e09f1f2
xA Development <xaaddon@gmail.com>
2026-03-25 15:04:29 +0100

    Create EntityMixin.java

---
+++ src/main/java/com/example/addon/mixin/EntityMixin.java
@@ -0,0 +1,16 @@
+package com.example.addon.mixins;
+
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+    
+    @Inject(method = "getTargetingMargin", at = @At("HEAD"), cancellable = true)
+    private void onGetTargetingMargin(CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(15.0f); 
+    }
+}
