6d5679df5eec1a133b9b2f005305fb9235dacdb3
xA Development <xaaddon@gmail.com>
2026-03-25 22:26:38 +0100

    Update and rename SimpleCrystal.java to xCrystal.java

---
+++ src/main/java/com/example/addon/modules/xCrystal.java
@@ -0,0 +1,49 @@
+package com.example.addon.modules;
+
+import com.example.addon.AddonTemplate;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.player.InvUtils;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.item.Items;
+
+public class xCrystal extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Double> range = sgGeneral.add(new DoubleSetting.Builder()
+        .name("range")
+        .defaultValue(5.0)
+        .min(1)
+        .sliderMax(15)
+        .build());
+
+    public xCrystal() {
+        super(AddonTemplate.CATEGORY, "xCrystal", "Crystal Aura");
+    }
+
+    @EventHandler
+    private void onTick(TickEvent.Pre event) {
+        if (mc.player == null) return;
+
+        var blockAttr = mc.player.getAttributeInstance(EntityAttributes.BLOCK_INTERACTION_RANGE);
+        var entityAttr = mc.player.getAttributeInstance(EntityAttributes.ENTITY_INTERACTION_RANGE);
+
+        if (blockAttr != null) blockAttr.setBaseValue(range.get());
+        if (entityAttr != null) entityAttr.setBaseValue(range.get());
+
+        handleDoubleHand();
+    }
+
+    private void handleDoubleHand() {
+        if (InvUtils.find(Items.TOTEM_OF_UNDYING).count() < 2) return;
+
+        if (mc.player.getOffHandStack().getItem() != Items.TOTEM_OF_UNDYING) {
+            InvUtils.move().from(InvUtils.find(Items.TOTEM_OF_UNDYING).slot()).toOffhand();
+        }
+        if (mc.player.getMainHandStack().getItem() != Items.TOTEM_OF_UNDYING) {
+            InvUtils.swap(InvUtils.find(Items.TOTEM_OF_UNDYING).slot(), false);
+        }
+    }
+}
