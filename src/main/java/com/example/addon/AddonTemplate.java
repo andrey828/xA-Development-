d318067d3306a64135817611fb0435cccb089127
xAndrey <andreymanea03@gmail.com>
2026-04-02 03:46:44 +0200

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -32,6 +32,7 @@ public class AddonTemplate extends MeteorAddon {
         m.add(new xCrystal());
         m.add(new xAnchor());
         m.add(new AdvancedParticles());
+        m.add(new AntiMace());
     }
     
 
